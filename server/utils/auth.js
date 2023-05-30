e6a1a062ac1eb573995ba1ff78032e42f6e01aa5
etapm <etap@ucdavis.edu>
2023-05-29 17:30:00 -0700

    authentification updated

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,18 +1,33 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
 
-module.exports = function (req, res, next) {
-  const token = req.header("x-auth-token");
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
 
-  if (!token) {
-    return res.status(401).json({ msg: "No token, authorization denied" });
-  }
+module.exports = {
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user;
-    next();
-  } catch (err) {
-    res.status(401).json({ msg: "Token is not valid" });
-  }
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+    }
+
+    return req;
+  },
+
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
 };
