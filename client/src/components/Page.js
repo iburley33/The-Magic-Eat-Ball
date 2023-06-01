491b4edb678261b6d67807a55e2678f9339075e3
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-05-31 20:10:53 -0700

    updated code in page

--- client/src/components/Page.js
+++ client/src/components/Page.js
@@ -1,40 +1,57 @@
-import React, { useState } from 'react';
-import Shake from './Shake'
-import FavoriteBtn from './FavoriteBtn';
+import React, { useState } from "react";
+import Shake from "./Shake";
+import FavoriteBtn from "./FavoriteBtn";
+import SignupForm from "./SignupForms";
+import LoginForm from "./LoginForms";
+import { Link } from "react-router-dom";
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function Page() {
-    const [name, setName] = useState('Click Shake');
-    const [address, setAddress] = useState('To find a random restaurant near you!')
-    return (
-        <div className="App">
-        <header>
-              <h1>MAGIC EAT-BALL</h1>
-              <div id="login-btns-container">
-                  <button className="btn" id="login-btn">Login</button>
-                  <button className="btn" id="signup-btn">Sign Up</button>
-              </div>
-          </header>
-          <section>
-              <div id="body-btns-container">
-              <button className="btn" id="only-faves-btn">Only show favorites?</button>
-              <button className="btn" id="view-faves-btn">View favorites</button>
-              <Shake setName={setName} setAddress={setAddress}/>
-              </div>
-              <div id="ball">
-                  <div id="window">
-                      <p id="restaurant-name">{name}</p>
-                      <p id="restaurant-address">{address}</p>
-                      <div id="triangle">
-                      </div>
-                  </div>
-              </div>
-            <FavoriteBtn name={name} address={address}/>
-              <script src="index.js"></script>
-          </section>
-      </div>
-    );
+  const [name, setName] = useState("Click Shake");
+  const [address, setAddress] = useState(
+    "To find a random restaurant near you!"
+  );
+  return (
+    <div className="App">
+      <header>
+        <h1>MAGIC EAT-BALL</h1>
+        <div id="login-btns-container">
+          <Link>
+            <button className="btn" id="login-btn">
+              Login
+            </button>
+          </Link>
+
+          <Link>
+            <button className="btn" id="signup-btn">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      </header>
+      <section>
+        <div id="body-btns-container">
+          <button className="btn" id="only-faves-btn">
+            Only show favorites?
+          </button>
+          <button className="btn" id="view-faves-btn">
+            View favorites
+          </button>
+          <Shake setName={setName} setAddress={setAddress} />
+        </div>
+        <div id="ball">
+          <div id="window">
+            <p id="restaurant-name">{name}</p>
+            <p id="restaurant-address">{address}</p>
+            <div id="triangle"></div>
+          </div>
+        </div>
+        <FavoriteBtn name={name} address={address} />
+        <script src="index.js"></script>
+      </section>
+    </div>
+  );
 }
 
 export default Page;
--- client/src/components/Shake.js
+++ client/src/components/Shake.js
@@ -1,11 +1,34 @@
 import React from "react";
-
-const shakeIt = function () {};
+import { apiCall } from "../utils/API";
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
-function Shake() {
-  return <button class="shake"></button>;
+function Shake({ setName, setAddress }) {
+  const shakeIt = async function () {
+    try {
+      const response = await apiCall();
+      console.log(response);
+      const num = response.results.length;
+      console.log(num);
+      const ind = Math.floor(Math.random() * num);
+      const restaurant = {
+        name: response.results[ind].name,
+        address: response.results[ind].location.address,
+      };
+      setName(restaurant.name);
+      setAddress(restaurant.address);
+      console.log(restaurant);
+      return restaurant;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <button onClick={shakeIt} id="shake-btn">
+      SHAKE
+    </button>
+  );
 }
 
 export default Shake;
