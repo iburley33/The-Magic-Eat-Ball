3493fd340fed09710952bd7b22d1ebe96c58844f
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-06-01 21:02:06 -0700

    added logout button and logout code, updated api, loginform, signin form, and page

--- client/src/components/LoginForms.js
+++ client/src/components/LoginForms.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
-import { createUser } from "../utils/API";
+import { loginUser } from "../utils/API";
 import Auth from "../utils/auth";
 //import { Link } from "react-router-dom";
 
 const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({ email: "", password: "" });
-  const [validated] = useState(false);
+  const [userFormData, setUserFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  //const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -16,14 +20,15 @@ const LoginForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    //const form = event.currentTarget;
+    //console.log(form);
+    //if (form.checkValidity() === false) {
+    //event.preventDefault();
+    //event.stopPropagation();
+    //}
 
     try {
-      const response = await createUser(userFormData);
+      const response = await loginUser(userFormData);
 
       if (!response.ok) {
         throw new Error("something went wrong!");
@@ -38,6 +43,7 @@ const LoginForm = () => {
     }
 
     setUserFormData({
+      username: "",
       email: "",
       password: "",
     });
@@ -45,7 +51,7 @@ const LoginForm = () => {
 
   return (
     <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+      <Form onSubmit={handleFormSubmit}>
         <Alert
           dismissible
           onClose={() => setShowAlert(false)}
@@ -54,6 +60,20 @@ const LoginForm = () => {
         >
           Something went wrong with your login credentials!
         </Alert>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="username">Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your username"
+            name="username"
+            onChange={handleInputChange}
+            value={userFormData.username}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Username is required!
+          </Form.Control.Feedback>
+        </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label htmlFor="email">Email</Form.Label>
           <Form.Control
@@ -94,4 +114,5 @@ const LoginForm = () => {
     </>
   );
 };
+console.log();
 export default LoginForm;
